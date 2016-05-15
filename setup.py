2cc6ffdc90e36efaed0daf0d942bcfedce07a906
Josh Junon <junon@uber.com>
2016-05-15 13:50:14 -0700

    prepare for PyPi upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'starfuse',
+    packages = ['starfuse'],
+    version = '0.1.0',
+    description = 'Mount StarBound .pak files as FUSE filesystems',
+    author = 'Josh Junon',
+    author_email = 'i.am.qix@gmail.com',
+    url = 'https://github.com/qix-/starfuse',
+    download_url = 'https://github.com/qix-/starfuse/tarball/0.1.0',
+    keywords = ['starfuse', 'starbound', 'fuse', 'pak', 'development', 'mod', 'extension'],
+    classifiers = [],
+)
